fe5390e4bf891d60d14fd77dafe12f4e9b30b3b2
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-05-27 21:54:59 +0530

    Time: 1 ms (72.6%), Space: 40.9 MB (55.31%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -3,20 +3,19 @@ class Solution {
         int len = word1.length()+ word2.length();
         char[] str = new char[len];
         int j = 0;
-        String strring = "";
 
         for(int i = 0; (i < word1.length() || i < word2.length()); i++){
             if(word1.length() > i){
-                strring += word1.charAt(i);
-                // j++;
+                str[j] = word1.charAt(i);
+                j++;
             }
             if(word2.length() > i){
-                strring += word2.charAt(i);
-                // j++;
+                str[j] = word2.charAt(i);
+                j++;
             }
         }
 
-        // String str1 = new String(str);
-        return strring;
+        String str1 = new String(str);
+        return str1;
     }
 }
