fda0c116c662ee8d9dee6805a4ac9c9f5ddcf890
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-05-27 21:54:28 +0530

    Time: 7 ms (21.51%), Space: 42.1 MB (29.29%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,36 +1,22 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String ans="";
+        int len = word1.length()+ word2.length();
+        char[] str = new char[len];
+        int j = 0;
+        String strring = "";
 
-        int i = 0;
-        while(i<word1.length() || i<word2.length()){
-
-            if(i<word1.length()){
-                ans += word1.charAt(i);
+        for(int i = 0; (i < word1.length() || i < word2.length()); i++){
+            if(word1.length() > i){
+                strring += word1.charAt(i);
+                // j++;
             }
-             if(i<word2.length()){
-               ans += word2.charAt(i);
+            if(word2.length() > i){
+                strring += word2.charAt(i);
+                // j++;
             }
-
-            i++;
         }
-        return ans;
-        // int len = word1.length()+ word2.length();
-        // char[] str = new char[len];
-        // int j = 0;
-
-        // for(int i = 0; (i < word1.length() || i < word2.length()); i++){
-        //     if(word1.length() > i){
-        //         str[j] = word1.charAt(i);
-        //         j++;
-        //     }
-        //     if(word2.length() > i){
-        //         str[j] = word2.charAt(i);
-        //         j++;
-        //     }
-        // }
 
         // String str1 = new String(str);
-        // return str1;
+        return strring;
     }
 }
