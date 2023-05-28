d388d60a375064956918ec0fcbd3af53292cc4f2
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-05-28 17:01:21 +0530

    Time: 1 ms (6.42%), Space: 41.4 MB (90.46%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,14 +3,6 @@ class Solution {
         
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
-                // if(matrix.length == 1 && matrix[i].length == 1){
-                //     if(matrix[0][0] == target){
-                //         return true;
-                //     }
-                //     else{
-                //         return false;
-                //     }
-                // }
                 int low = 0, high = matrix[i].length-1;
 
                 while(low < matrix.length && high >= 0){
