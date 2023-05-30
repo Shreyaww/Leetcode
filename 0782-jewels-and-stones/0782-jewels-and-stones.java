a0de3aff83d13ca8683faec961f8f156ef6e0ce9
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-05-30 17:17:37 +0530

    Time: 1 ms (91.24%), Space: 40.9 MB (67.76%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,16 +1,26 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        HashMap<Character, Integer> map = new HashMap<>();
-
+        int num = 0;
         for(int i = 0; i < stones.length(); i++){
-            map.put(stones.charAt(i), map.getOrDefault(stones.charAt(i), 0) + 1);
+            for(int j = 0; j < jewels.length(); j++){
+                if(jewels.charAt(j) == stones.charAt(i)){
+                    num++;
+                }
+            }
         }
-        System.out.println(map);
+        return num;
+    //If I solve the sum using hashmap the beats reduce to 5% of Java Solutions, Hence Brute F
+        // HashMap<Character, Integer> map = new HashMap<>();
 
-        int count = 0;
-        for(int i = 0; i < jewels.length(); i++){
-            count += map.getOrDefault(jewels.charAt(i), 0);
-        }
-        return count;
+        // for(int i = 0; i < stones.length(); i++){
+        //     map.put(stones.charAt(i), map.getOrDefault(stones.charAt(i), 0) + 1);
+        // }
+        // System.out.println(map);
+
+        // int count = 0;
+        // for(int i = 0; i < jewels.length(); i++){
+        //     count += map.getOrDefault(jewels.charAt(i), 0);
+        // }
+        // return count;
     }
 }
