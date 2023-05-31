836087378412a73da637811f04939a204294145d
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-05-31 17:02:44 +0530

    Time: 3 ms (41.08%), Space: 44.3 MB (63.75%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -5,12 +5,11 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == 1){
                 count++;
+                max = Math.max(max, count);
             }
             else{
                 count = 0;
             }
-
-            max = Math.max(max, count);
         }
         return max;
     }
