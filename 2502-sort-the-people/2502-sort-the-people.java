d150418434932125708b2f26344b631791eb2a6c
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-05-31 16:31:36 +0530

    Time: 71 ms (5.78%), Space: 44.9 MB (5.36%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,33 +1,53 @@
 class Solution {
+    //I literally did brute force on this!!! OMG
     public String[] sortPeople(String[] names, int[] heights) {
-        for(int i = 0; i < names.length -1; i++){
-            for(int j = i+1; j < names.length; j++){
-                if(heights[j] > heights[i]){
-                    names = swap_str(names, i, j);
-                    heights = swap_int(heights, i, j);
-                }
-            }
+        HashMap<Integer, String> map = new HashMap<>();
+
+        for(int i = 0; i < names.length; i++){
+            map.put(heights[i], names[i]);
         }
-        return names;
+
+        Arrays.sort(heights);
+        String[] sorted = new String[names.length];
+        for(int i = 0; i < names.length; i++){
+            System.out.println(map.get(heights[i]));
+        }
+
+        int j = 0;
+        for(int i = names.length-1; i >= 0; i--){
+            sorted[j++] = map.get(heights[i]);
+        }
+
+        System.out.println(map);
+        return sorted;
+        // for(int i = 0; i < names.length -1; i++){
+        //     for(int j = i+1; j < names.length; j++){
+        //         if(heights[j] > heights[i]){
+        //             names = swap_str(names, i, j);
+        //             heights = swap_int(heights, i, j);
+        //         }
+        //     }
+        // }
+        // return names;
     }
 
-    public static int[] swap_int(int[] heights, int i, int j){
-        int int_temp = 0;
+    // public static int[] swap_int(int[] heights, int i, int j){
+    //     int int_temp = 0;
 
-        int_temp = heights[j];
-        heights[j] = heights[i];
-        heights[i] = int_temp;
+    //     int_temp = heights[j];
+    //     heights[j] = heights[i];
+    //     heights[i] = int_temp;
 
-        return heights;
-    }
+    //     return heights;
+    // }
 
-    public static String[] swap_str(String[] names, int i, int j){
-        String temp = "";
+    // public static String[] swap_str(String[] names, int i, int j){
+    //     String temp = "";
 
-        temp = names[j];
-        names[j] = names[i];
-        names[i] = temp;
+    //     temp = names[j];
+    //     names[j] = names[i];
+    //     names[i] = temp;
 
-        return names;
-    }
+    //     return names;
+    // }
 }
