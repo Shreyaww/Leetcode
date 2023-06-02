06623601687aa3c7cea6152d59780c75b8f65038
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-02 15:44:15 +0530

    Time: 85 ms (5.4%), Space: 58.4 MB (20.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,25 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] == nums[i+1]){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+            
+        }
+        System.out.println(map);
+        for(int i = 0; i < nums.length; i++){
+            if(map.getOrDefault(nums[i], 0) > 1){
                 return true;
             }
         }
+
+        
+        // Arrays.sort(nums);
+        // for(int i = 0; i < nums.length-1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return true;
+        //     }
+        // }
         return false;
     }
 }
