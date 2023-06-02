8408f4ffee21da15af7ca14418f874934f8df89e
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-02 15:48:18 +0530

    Time: 13 ms (51.1%), Space: 55.2 MB (83.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,11 +3,15 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-
-            if(map.getOrDefault(nums[i], 0) > 1){
+            if(map.containsKey(nums[i])){
                 return true;
-            }    
+            }
+            map.put(nums[i], 1);
+            // map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+
+            // if(map.getOrDefault(nums[i], 0) > 1){
+            //     return true;
+            // }    
         }
         return false;
     }
