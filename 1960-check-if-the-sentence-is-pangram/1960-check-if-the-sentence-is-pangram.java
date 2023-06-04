73f67d46444b02b338a36a8d099abb9856741c99
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-04 17:36:07 +0530

    Time: 8 ms (6.58%), Space: 40.8 MB (62.69%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -6,25 +6,19 @@ class Solution {
         for(int i = 0; i < 26; i++){
             map.put(c++, 0);
         }
-        System.out.println(map);
 
         for(int i = 0; i < sentence.length(); i++){
             if(map.containsKey(sentence.charAt(i))){
                 map.replace(sentence.charAt(i),1);
             } 
         }
-        System.out.println(map);
+
         for(Map.Entry<Character, Integer> maps : map.entrySet()){
-            //Character ch = maps.getKey();
             int n = maps.getValue();
             if(n == 0){
                 return false;
             }
         }
-        // System.out.println(map);
-        // if(sentence.length() >= 26){
-        //     return true;
-        // }
         return true;
     }
 }
