0fddc5a31f01b32645526c1728bba416d14384d2
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-06 18:56:58 +0530

    Time: 3 ms (41.79%), Space: 44.7 MB (35.55%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] arr = new int[2];
         int i = 0;
         while(i < nums.length){
             int correct = nums[i]-1;
@@ -14,11 +13,10 @@ class Solution {
 
         for(int j = 0; j < nums.length; j++){
             if(nums[j] != j+1){
-                arr[0] = nums[j];
-                arr[1] = j+1;
+                return new int[]{nums[j], j+1};
             }
         }
-        return arr;
+        return new int[]{};
     }
 
     public void swap(int[] arr, int i, int correct){
