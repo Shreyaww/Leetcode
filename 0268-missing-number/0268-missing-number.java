cd559b32aef3cc3917ad0e7f7124d10a99b6b9fa
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-06 17:43:11 +0530

    Time: 1 ms (42.27%), Space: 44.3 MB (15.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,27 +1,56 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        return search(nums);
-    }
+        int[] arr = new int[nums.length+1];
+        for(int i = 0; i < arr.length-1; i++){
+            arr[i] = nums[i];
+        }
 
-    public int search(int[] nums){
-        int start = 0;
-        int end = nums.length;
-        Arrays.sort(nums);
-        
-        while(start <= end){
-            for(int i = 0; i < nums.length; i++){
-                if(nums[start] != i){
-                    return i;
-                }
-                else if(nums[end-1] == nums.length-i){
-                    start++;
-                    end--;
-                }
-                else{
-                    return (nums.length-i);
-                }   
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i];
+            if(arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }
+            else{
+                i++;
             }
         }
+    
+        for(int j = 0; j < arr.length; j++){
+            if(arr[j] == 0 && j != 0){
+                return j;
+            }
+        }
+        
         return 0;
+       // return search(nums);
     }
+
+    public void swap(int[] nums, int i, int correct){
+        int temp = nums[i];
+        nums[i] = nums[correct];
+        nums[correct] = temp;
+    } 
+
+    // public int search(int[] nums){
+    //     int start = 0;
+    //     int end = nums.length;
+    //     Arrays.sort(nums);
+        
+    //     while(start <= end){
+    //         for(int i = 0; i < nums.length; i++){
+    //             if(nums[start] != i){
+    //                 return i;
+    //             }
+    //             else if(nums[end-1] == nums.length-i){
+    //                 start++;
+    //                 end--;
+    //             }
+    //             else{
+    //                 return (nums.length-i);
+    //             }   
+    //         }
+    //     }
+    //     return 0;
+    // }
 }
