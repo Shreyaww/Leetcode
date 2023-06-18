61b8baba1140da526ca505e18d6f93d7b071a1dc
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-18 14:16:13 +0530

    Time: 9 ms (99.37%), Space: 43.2 MB (16.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,32 +1,34 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        StringBuilder str = new StringBuilder(s).reverse();
-      //  str.reverse();
+    //  //   Method 1 : 
+    //     String s = String.valueOf(x);
+    //     StringBuilder str = new StringBuilder(s).reverse();
 
-        if(str.toString().equals(s)){
-            return true;
+    //     if(str.toString().equals(s)){
+    //         return true;
+    //     }
+    //     return false;
+
+//Method 2 : 
+        int reverse = 0;
+        int remainder = 0;
+        boolean bool = false;
+        int temp = x;
+
+        while(temp!=0){
+            remainder = temp%10;
+            reverse = reverse*10 + remainder;
+            temp = temp/10;
         }
-        return false;
-        // int reverse = 0;
-        // int remainder = 0;
-        // boolean bool = false;
-        // int temp = x;
+        //System.out.println(reverse);
+        if(reverse == x){
+            bool = true;
+        }
 
-        // while(temp!=0){
-        //     remainder = temp%10;
-        //     reverse = reverse*10 + remainder;
-        //     temp = temp/10;
-        // }
-        // //System.out.println(reverse);
-        // if(reverse == x){
-        //     bool = true;
-        // }
+        if(x < 0 && reverse < 0){
+            bool = false;
+        }
 
-        // if(x < 0 && reverse < 0){
-        //     bool = false;
-        // }
-
-        // return bool;
+        return bool;
     }
 }
