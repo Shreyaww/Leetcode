72f281ab9872c997c68efade21880c8beca95de5
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-18 14:17:04 +0530

    Time: 9 ms (99.37%), Space: 42.8 MB (42.61%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -10,25 +10,30 @@ class Solution {
     //     return false;
 
 //Method 2 : 
-        int reverse = 0;
-        int remainder = 0;
-        boolean bool = false;
-        int temp = x;
-
-        while(temp!=0){
-            remainder = temp%10;
-            reverse = reverse*10 + remainder;
-            temp = temp/10;
+        if(x < 0){
+            return false;
         }
+        else{
+            int reverse = 0;
+            int remainder = 0;
+            boolean bool = false;
+            int temp = x;
+
+            while(temp!=0){
+                remainder = temp%10;
+                reverse = reverse*10 + remainder;
+                temp = temp/10;
+            }
         //System.out.println(reverse);
-        if(reverse == x){
-            bool = true;
-        }
+            if(reverse == x){
+                bool = true;
+            }
 
-        if(x < 0 && reverse < 0){
-            bool = false;
-        }
+            if(x < 0 && reverse < 0){
+                bool = false;
+            }
 
-        return bool;
+            return bool;
+        }
     }
 }
