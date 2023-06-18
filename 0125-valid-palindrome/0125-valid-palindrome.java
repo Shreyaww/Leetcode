b8b355d3a59b0d4cb449872b96c590efec476634
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-18 13:50:02 +0530

    Time: 28 ms (24.67%), Space: 44 MB (23.88%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        System.out.println(s);
 
         int start = 0, end = s.length()-1;
 
