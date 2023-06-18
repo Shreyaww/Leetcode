a76ee0f0753445f33ce1e110b1fd5e04c75aede7
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-18 15:15:54 +0530

    Time: 1 ms (89.53%), Space: 51.7 MB (5.36%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,5 +1,6 @@
 class Solution {
     public void reverseString(char[] s) {
+        //Method 1:
         // char[] arr = new char[s.length];
         // int k = 0;
         // for(int i = s.length-1; i >= 0; i--){
@@ -10,16 +11,32 @@ class Solution {
         //     s[i] = arr[i];
         // }
 
-        int start = 0, end = s.length-1;
+        //Method 2:
+        // int start = 0, end = s.length-1;
 
-        while(start <= end){
-            swap(s, start++, end--);
-        }
+        // while(start <= end){
+        //     swap(s, start++, end--);
+        // }
+
+
+        //Method 3 : recursion
+        recursion(s, 0, s.length-1);
     }
 
-    public void swap(char[] s, int start, int end){
+    public void recursion(char[] s, int start, int end){
+        if(start >= end){
+            return;
+        }
         char temp = s[start];
         s[start] = s[end];
         s[end] = temp;
+
+        recursion(s, start+1, end-1);
     }
+
+    // public void swap(char[] s, int start, int end){
+    //     char temp = s[start];
+    //     s[start] = s[end];
+    //     s[end] = temp;
+    //}
 }
