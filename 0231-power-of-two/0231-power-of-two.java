df6db7c3d7be93f05ca07bb18e884563f534da30
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-19 14:16:21 +0530

    Time: 1 ms (100%), Space: 40.1 MB (33.66%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,19 +1,32 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n == 1){
+        
+        return func(n, 1);
+        // if(n == 1){
+        //     return true;
+        // }
+
+        // if(n%2 != 0){
+        //         return false;
+        // }
+        // long result = 1;
+        // for(int i = 1; i <= n; i++){
+        //     if(n == result){
+        //         return true;
+        //     }
+        //     result = result * 2;
+        // }
+        // return false;
+    }
+
+    public boolean func(int n, long result){
+        if(result == n){
             return true;
         }
-
-        if(n%2 != 0){
-                return false;
+        else if(n%2 != 0 || result > n){
+            return false;
         }
-        long result = 1;
-        for(int i = 1; i <= n; i++){
-            if(n == result){
-                return true;
-            }
-            result = result * 2;
-        }
-        return false;
+        result *= 2;
+        return func(n, result);
     }
 }
