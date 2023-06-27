84880eb236a1153d18d53ba03ebfce9a00d867ad
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-06-27 15:04:48 +0530

    Time: 4 ms (6.5%), Space: 44.8 MB (25.85%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,30 +1,53 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[][] arr = new int[matrix.length][matrix[0].length];
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
 
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                arr[i][j] = matrix[i][j];
-            }
-        }
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
                 if(matrix[i][j] == 0){
-                    for(int k = 0; k < matrix.length; k++){
-                        arr[k][j] = 0;
-                    }
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        
+        System.out.println(Arrays.toString(row));
+        System.out.println(Arrays.toString(col));
 
-                    for(int k = 0; k < matrix[0].length; k++){
-                        arr[i][k] = 0;
-                    }
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
                 }
             }
         }
 
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                matrix[i][j] = arr[i][j];
-            }
-        }
+        // int[][] arr = new int[matrix.length][matrix[0].length];
+
+        // for(int i = 0; i < matrix.length; i++){
+        //     for(int j = 0; j < matrix[0].length; j++){
+        //         arr[i][j] = matrix[i][j];
+        //     }
+        // }
+        // for(int i = 0; i < matrix.length; i++){
+        //     for(int j = 0; j < matrix[0].length; j++){
+        //         if(matrix[i][j] == 0){
+        //             for(int k = 0; k < matrix.length; k++){
+        //                 arr[k][j] = 0;
+        //             }
+
+        //             for(int k = 0; k < matrix[0].length; k++){
+        //                 arr[i][k] = 0;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // for(int i = 0; i < matrix.length; i++){
+        //     for(int j = 0; j < matrix[0].length; j++){
+        //         matrix[i][j] = arr[i][j];
+        //     }
+        // }
     }
 }
