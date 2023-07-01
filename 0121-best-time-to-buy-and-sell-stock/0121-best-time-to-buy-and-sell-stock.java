b24c1a740abb218ffa69aa517ee1027b869da849
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-01 17:38:46 +0530

    Time: 3 ms (18.29%), Space: 61.1 MB (41.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -10,28 +10,6 @@ class Solution {
         }
 
         return profit;
-        // int max = 0;
-        // int profit = 0;
-
-        // for(int i = 0; i < prices.length-1; i++){
-        //     int max1 = prices[i];
-        //     for(int j = i+1; j < prices.length; j++){
-        //         max = Math.max(max, prices[j]-max1);
-        //     }
-        // }
-        // return max;
-
-        // int profit = 0;
-
-        // for(int i = 0; i < prices.length; i++){
-        //     int buy = prices[i];
-        //     int max = buy;
-        //     for(int j = i+1; j < prices.length; j++){
-        //         max = Math.max(max, prices[j]);
-        //     }
-
-        //     profit = Math.max(max-buy, profit);
-        // }
-        // return profit;
+        
     }
 }
