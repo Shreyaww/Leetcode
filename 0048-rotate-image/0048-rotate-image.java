f0523fdb6de38b8b23dbdd9698a0ae1cc3b8e0b8
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-01 17:51:56 +0530

    Time: 0 ms (100%), Space: 41.3 MB (71.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -3,20 +3,19 @@ class Solution {
         int[][] arr = new int[matrix.length][matrix[0].length];
 
         for(int i = 0; i < matrix.length; i++){
-            int[] arr1 = new int[matrix.length];
-            for(int j = 0; j < matrix[0].length; j++){
-                arr1[j] = matrix[i][j];
-            }
+            // int[] arr1 = new int[matrix.length];
+            // for(int j = 0; j < matrix[0].length; j++){
+            //     arr1[j] = matrix[i][j];
+            // }
 
-            int k = 0;
-            for(int j = 0; j < matrix[0].length; j++){
-                arr[j][matrix[0].length - i-1] = arr1[k++];
-            }
-            //System.out.println(Arrays.toString(arr1));
-        }
+            // int k = 0;
+            // for(int j = 0; j < matrix[0].length; j++){
+            //     arr[j][matrix[0].length - i-1] = arr1[k++];
+            // }
 
-        for(int i = 0; i < matrix.length; i++){
-            System.out.println(Arrays.toString(arr[i]));
+            for(int j = 0; j < matrix[0].length; j++){
+                arr[j][matrix[0].length - i-1] = matrix[i][j];
+            }
         }
 
         for(int i = 0; i < matrix.length; i++){
