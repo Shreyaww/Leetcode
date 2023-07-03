a5abb07f7f4ba61e59bf26f8d24ee9a901b7c49b
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-03 16:20:20 +0530

    Time: 10 ms (44.76%), Space: 46.7 MB (25.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -24,8 +24,6 @@ class Solution {
             }
         }
 
-        System.out.println(ls);
-
         return arr;
         // Arrays.sort(intervals, new Comparator<int[]>(){
         //     public int compare(int[] a, int[] b){
