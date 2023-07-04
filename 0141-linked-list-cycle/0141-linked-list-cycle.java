e6cfb50bd653cacd9fa6131a515c2f39dac23ff3
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-04 16:54:08 +0530

    Time: 0 ms (100%), Space: 43.6 MB (62.59%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,15 +11,34 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode node) {
-        HashSet<ListNode> set = new HashSet<>();
+        //Optimal -
+        if(node == null || node.next == null){
+            return false;
+        }
+        
+        ListNode slow = node;
+        ListNode fast = node;
 
-        while(node != null){
-            set.add(node);
-            node = node.next;
-            if(set.contains(node)){
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
                 return true;
             }
         }
+
         return false;
+        //Brute Force 1 - 
+        // HashSet<ListNode> set = new HashSet<>();
+
+        // while(node != null){
+        //     set.add(node);
+        //     node = node.next;
+        //     if(set.contains(node)){
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 }
