40fb35fe150d665d8ad03eddb03f8ece9303da35
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-04 16:31:49 +0530

    Time: 5 ms (42.53%), Space: 44.4 MB (11.4%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -5,8 +5,7 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
-        System.out.println(map);
-
+        
         for(int i = 0; i < nums.length; i++){
             if(map.get(nums[i]) == 1){
                 return nums[i];
