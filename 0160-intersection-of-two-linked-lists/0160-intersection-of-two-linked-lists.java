68b6b96eaea61718be288ef77cbc57ca3544a2ac
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-04 15:48:28 +0530

    Time: 1 ms (100%), Space: 47 MB (34.63%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,19 +11,64 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashSet<ListNode> ls = new HashSet<>();
+        //Optimal - 
+        ListNode dummy1 = headA;
+        ListNode dummy2 = headB;
+        int countA = 0, countB = 0;
 
-        while(headA != null){
-            ls.add(headA);
-            headA = headA.next;
+        while(dummy1 != null){
+            countA++;
+            dummy1 = dummy1.next;
         }
-        while(headB != null){
-            if(ls.contains(headB)){
-                return headB;
+        // System.out.println(countA);
+
+        while(dummy2 != null){
+            countB++;
+            dummy2 = dummy2.next;
+        }
+        // System.out.println(countB);
+        int diff = Math.abs(countA - countB);
+        // System.out.println(diff);
+        dummy1 = headA;
+        dummy2 = headB;
+
+        if(countA > countB){
+            while(diff != 0){
+                dummy1 = dummy1.next;
+                diff--;
             }
-            headB = headB.next;
         }
+        else if(countB > countA){
+            while(diff != 0){
+                dummy2 = dummy2.next;
+                diff--;
+            }
+        }
+        
+        while(dummy1 != null || dummy2 != null){
+            if(dummy1 == dummy2){
+                return dummy1;
+            }
+            dummy1 = dummy1.next;
+            dummy2 = dummy2.next;
+        }
+        
         return null;
+
+        //Better - 
+        // HashSet<ListNode> ls = new HashSet<>();
+
+        // while(headA != null){
+        //     ls.add(headA);
+        //     headA = headA.next;
+        // }
+        // while(headB != null){
+        //     if(ls.contains(headB)){
+        //         return headB;
+        //     }
+        //     headB = headB.next;
+        // }
+        // return null;
 
         //Brute Force - 
         // while(headA != null){
