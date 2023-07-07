b5edda8e4d294c02f537e6d9925bb7985fb5c72d
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-07 16:51:13 +0530

    Time: 13 ms (33.72%), Space: 46.2 MB (90.69%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -56,9 +56,9 @@ class Solution {
             if(map.get(nums[i]) > flr && !ls.contains(nums[i])){
                 ls.add(nums[i]);
             }
-            else if(ls.size() == 2){
-                return ls;
-            }
+            // else if(ls.size() == 2){
+            //     return ls;
+            // }
         }
 
         return ls;
