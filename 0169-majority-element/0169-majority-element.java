c3552b048471784378ac772f43485cccfe7eba2e
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-07 15:49:30 +0530

    Time: 11 ms (37.96%), Space: 47.1 MB (68.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
         //Soln3 (Brute Force)- 
-        int size = nums.length/2;
+        // int size = nums.length/2;
 
-        for(int i = 0; i < nums.length; i++){
-            int count = 0;
-            for(int j = 0; j < nums.length; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
-            if(count > size){
-                return nums[i];
-            }
-        }
+        // for(int i = 0; i < nums.length; i++){
+        //     int count = 0;
+        //     for(int j = 0; j < nums.length; j++){
+        //         if(nums[i] == nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count > size){
+        //         return nums[i];
+        //     }
+        // }
         
-        return 0;
+        // return 0;
 
         //Soln2- 
         // Arrays.sort(nums);
@@ -24,12 +24,20 @@ class Solution {
 
 
         //Soln 1 - 
-        // HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        // for(int val: nums){
-        //     map.put(val, map.getOrDefault(val, 0)+1);
-        // }
-        
+        for(int val: nums){
+            map.put(val, map.getOrDefault(val, 0)+1);
+        }
+        int size = nums.length/2;
+        System.out.println(map);
+        for(int i = 0; i < nums.length; i++){
+            if(map.get(nums[i]) > size){
+                return nums[i];
+            }
+        }
+
+        return 0;
         // int max = 0;
         // int key = -1;
         // for(Map.Entry<Integer, Integer> maps : map.entrySet()){
