e8a3e165acb28a6b968523f44d25d33c76190b5a
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-07 15:43:50 +0530

    Time: 1436 ms (5.04%), Space: 48.4 MB (54.5%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,9 +4,9 @@ class Solution {
         int size = nums.length/2;
 
         for(int i = 0; i < nums.length; i++){
-            int count = 1;
+            int count = 0;
             for(int j = 0; j < nums.length; j++){
-                if(nums[i] == nums[j] && i!= j){
+                if(nums[i] == nums[j]){
                     count++;
                 }
             }
