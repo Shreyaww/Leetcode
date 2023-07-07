8861b6e968d4eb4960f77a1c9e9b424888c6b704
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-07 15:42:02 +0530

    Time: 2377 ms (5.04%), Space: 48.7 MB (38.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,26 +1,48 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        //Soln3 - 
+        int size = nums.length/2;
 
-        // Arrays.sort(nums);
-        // return nums[nums.length/2];
-        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int count = 1;
+            for(int j = 0; j < nums.length; j++){
+                if(nums[i] == nums[j] && i!= j){
+                    count++;
+                }
 
-        for(int val: nums){
-            map.put(val, map.getOrDefault(val, 0)+1);
-        }
-        
-        int max = 0;
-        int key = -1;
-        for(Map.Entry<Integer, Integer> maps : map.entrySet()){
-            int val = maps.getValue(); 
-            int key1 = maps.getKey(); 
-
-            if(val > max){
-                key = key1; 
-                max = val;  
+                if(count > size){
+                    return nums[i];
+                }
             }
         }
+        
+        return 0;
+
+        //Soln2- 
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
+
+
+
+        //Soln 1 - 
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        // for(int val: nums){
+        //     map.put(val, map.getOrDefault(val, 0)+1);
+        // }
+        
+        // int max = 0;
+        // int key = -1;
+        // for(Map.Entry<Integer, Integer> maps : map.entrySet()){
+        //     int val = maps.getValue(); 
+        //     int key1 = maps.getKey(); 
+
+        //     if(val > max){
+        //         key = key1; 
+        //         max = val;  
+        //     }
+        // }
     
-        return key;
+        //return key;
     }
 }
