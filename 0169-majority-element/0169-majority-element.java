43a6246a11c9a5c12607e8b249f22f3e58aa446c
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-07 16:05:22 +0530

    Time: 1 ms (99.76%), Space: 48.9 MB (21.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        //Optimal Solution(Moore's Solution) -
+        int ele = nums[0], count = 0;
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == ele){
+                count++;
+            }
+            else if(count == 0){
+                ele = nums[i];
+            }
+            else{
+                count--;
+            }
+        }
+
+        return ele;
         //Soln3 (Brute Force)- 
         // int size = nums.length/2;
 
@@ -23,20 +39,20 @@ class Solution {
 
 
 
-        //Soln 1 - 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int size = nums.length/2;
+        //Better Solution - 
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // int size = nums.length/2;
 
-        for(int val: nums){
-            map.put(val, map.getOrDefault(val, 0)+1);
-        }
+        // for(int val: nums){
+        //     map.put(val, map.getOrDefault(val, 0)+1);
+        // }
         
-        for(int i = 0; i < nums.length; i++){
-            if(map.get(nums[i]) > size){
-                return nums[i];
-            }
-        }
+        // for(int i = 0; i < nums.length; i++){
+        //     if(map.get(nums[i]) > size){
+        //         return nums[i];
+        //     }
+        // }
 
-        return 0;
+        // return 0;
     }
 }
