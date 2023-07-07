384410e5c34e23c476bf0df7e995ac70de42871f
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-07 16:30:57 +0530

    Time: 13 ms (33.72%), Space: 46.5 MB (76.07%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         //Brute Force - 
-        ArrayList<Integer> ls = new ArrayList<>();
+        // ArrayList<Integer> ls = new ArrayList<>();
 
-        for(int i = 0; i < nums.length; i++){
-            int count = 0;
-            for(int j = 0; j < nums.length; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
-            if(count > nums.length/3 && !ls.contains(nums[i])){
-                ls.add(nums[i]);
-            }
-        }
+        // for(int i = 0; i < nums.length; i++){
+        //     int count = 0;
+        //     for(int j = 0; j < nums.length; j++){
+        //         if(nums[i] == nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count > nums.length/3 && !ls.contains(nums[i])){
+        //         ls.add(nums[i]);
+        //     }
+        // }
 
-        return ls;
+        // return ls;
+
+
+
         // int flr = nums.length/3;
         // ArrayList<Integer> ls = new ArrayList<>();
         
@@ -38,22 +41,23 @@ class Solution {
         // }
         // return ls;
 
-        // int size = nums.length;
-        // int flr = size/3;
+        //Using HashMap - 
+        int size = nums.length;
+        int flr = size/3;
 
-        // ArrayList<Integer> ls = new ArrayList<>();
-        // HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> ls = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        // for(int i = 0; i < size; i++){
-        //     map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-        // }
+        for(int i = 0; i < size; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
 
-        // for(int i = 0; i < size; i++){
-        //     if(map.get(nums[i]) > flr && !ls.contains(nums[i])){
-        //         ls.add(nums[i]);
-        //     }
-        // }
+        for(int i = 0; i < size; i++){
+            if(map.get(nums[i]) > flr && !ls.contains(nums[i])){
+                ls.add(nums[i]);
+            }
+        }
 
-        // return ls;
+        return ls;
     }
 }
