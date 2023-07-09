4754df6bdc9516828518c816b65f55cb1330752c
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-09 16:01:48 +0530

    Time: 2 ms (91.74%), Space: 40.4 MB (86.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,47 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stk = new Stack<Character>();
-        char[] c = s.toCharArray();
-    
+
         for(int i = 0; i < s.length(); i++){
-            if(c[i] == '(')
-                stk.push(')');
-            else if(c[i] == '[')
-                stk.push(']');
-            else if(c[i] == '{')
-                stk.push('}');
-            else if(stk.isEmpty() || stk.pop() != c[i]){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stk.push(s.charAt(i));
+            }
+            else if(!stk.isEmpty()){
+                if(stk.peek() == '(' && s.charAt(i) == ')'){
+                    stk.pop();
+                }
+                else if(stk.peek() == '[' && s.charAt(i) == ']'){
+                    stk.pop();
+                }
+                else if(stk.peek() == '{' && s.charAt(i) == '}'){
+                    stk.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
                 return false;
-            }    
+            }
+            
         }
+
         return stk.isEmpty();
+
+        // Stack<Character> stk = new Stack<Character>();
+        // char[] c = s.toCharArray();
+    
+        // for(int i = 0; i < s.length(); i++){
+        //     if(c[i] == '(')
+        //         stk.push(')');
+        //     else if(c[i] == '[')
+        //         stk.push(']');
+        //     else if(c[i] == '{')
+        //         stk.push('}');
+        //     else if(stk.isEmpty() || stk.pop() != c[i]){
+        //         return false;
+        //     }    
+        // }
+        // return stk.isEmpty();
     }
 }
