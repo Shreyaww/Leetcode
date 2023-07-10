6ce36c53b08b976e989f76ac055ff9a9c49bda73
Shreya Singh <88741590+Shreyaww@users.noreply.github.com>
2023-07-10 15:05:00 +0530

    Time: 3 ms (91.8%), Space: 43.7 MB (50.55%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,39 +1,42 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stk = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int[] ans = new int[nums1.length];
 
+        for(int i = 0; i < nums2.length; i++){
+            while(!stk.isEmpty() && nums2[i] > stk.peek()){
+                map.put(stk.pop(), nums2[i]);
+            }
+            stk.add(nums2[i]);
+        }
+        
         for(int i = 0; i < nums1.length; i++){
-            ans[i] = -1;
-            for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    for(int k = j+1; k < nums2.length; k++){
-                        if(nums2[k] > nums2[j]){
-                            ans[i] = nums2[k];
-                            break;
-                        }
-                    }
-                }
+            if(map.containsKey(nums1[i])){
+                ans[i] = map.get(nums1[i]);
+            }
+            else{
+                ans[i] = -1;
             }
         }
+       
         return ans;
+
         // int[] ans = new int[nums1.length];
 
         // for(int i = 0; i < nums1.length; i++){
+        //     ans[i] = -1;
         //     for(int j = 0; j < nums2.length; j++){
-        //         if(nums1[i] == nums2[j] && j == nums2.length-1){
-        //             ans[i] = -1;
-        //         }
-        //         else if(nums1[i] == nums2[j]){
-        //             if(nums2[j] < nums2[j+1]){
-        //                 ans[i] = nums2[j+1];
-        //             }
-        //             else{
-        //                 ans[i] = -1;
+        //         if(nums1[i] == nums2[j]){
+        //             for(int k = j+1; k < nums2.length; k++){
+        //                 if(nums2[k] > nums2[j]){
+        //                     ans[i] = nums2[k];
+        //                     break;
+        //                 }
         //             }
         //         }
         //     }
         // }
-
         // return ans;
     }
 }
